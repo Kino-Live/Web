023cf2494ee68480f6a85d7bf64e11ab58d09ca7
NureIvanilovIllia <illia.ivanilov@nure.ua>
2025-10-11 19:46:30 +0300

    Created registration page

---
+++ src/app/registration/page.tsx
@@ -0,0 +1,19 @@
+import Logo from "@/components/ui/logo";
+
+export default function RegisterPage() {
+    return (
+        <main className="flex h-screen">
+            <div className="flex-1 flex flex-col justify-between p-10 font-extralight italic bg-gradient-to-r from-white to-white/40 bg-clip-text text-transparent">
+                <Logo />
+                <div className="text-7xl">
+                    Welcome. <br />
+                    Begin your cinematic adventure now with our
+                    ticketing platform.
+                </div>
+            </div>
+
+            <div className="flex-1 bg-white flex flex-col justify-center items-center">
+            </div>
+        </main>
+    );
+}
---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Logo() {
+    return <Image src="/Logo.svg" alt="Logo" width={115} height={101} />;
+}
